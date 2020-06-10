6211eb1377b39dad32b94f93eab681ffe2de0f60
Alejandro Medina <5almero5@gmail.com>
2020-06-10 13:56:51 -0500

    add templates

---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+ const Header = () => {
+     const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href="/">
+                        icon.ico
+                    </a>
+                </h1>
+            </div>
+            <nav class="Header-nav">
+                <a href="#/about/">
+                    About
+                </a>
+            </nav>
+        </div>
+     `;
+     return view;
+ }
+
+export default Header;
