e0a1f1106a181052ae6990d723ea7d00cd975992
Alejandro Medina <5almero5@gmail.com>
2020-06-11 17:35:40 -0500

    add rotue logical

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import router from './routes';
+
 window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,5 +1,6 @@
 const getHash = () => 
     location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
      // ['', '1',']
+     // ['', 'home',']
 
 export default getHash;
