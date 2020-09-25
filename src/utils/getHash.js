bef02d305b60a29ec9e6f8d0de807aec0480bbbf
Alejandro Medina <5almero5@gmail.com>
2020-09-25 00:46:03 -0500

    fix route hash

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,4 +1,4 @@
-const getHash = () =>
- location.hash.replace(/#\/([a-z]+[-?'?]?[a-z]+)\/?/, "$1").toLowerCase() || '/'
+const getHash = () => 
+    location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
 
-export default getHash
+export default getHash;
