6fc963934c009e606d53f5513553af34aca8c020
Alejandro Medina <5almero5@gmail.com>
2020-09-24 21:51:24 -0500

    Add  logo image

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,10 +1,11 @@
+import Logo from '../assets/logo.png';
  const Header = () => {
      const view = `
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
                     <a href="/">
-                        icon.ico
+                        <img src=${Logo}>
                     </a>
                 </h1>
             </div>
--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,6 +1,4 @@
 const getHash = () => 
     location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
-     // ['', '1',']
-     // ['', 'home',']
 
 export default getHash;
